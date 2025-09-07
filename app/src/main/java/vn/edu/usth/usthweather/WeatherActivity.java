bee4acbc7f5013c717dab2e0cffa9c64fb9dc9bc
Ngan Pham <nganpt.23bi14334@usth.edu.vn>
2025-09-07 11:59:43 +0700

    Practical Work 2

---
+++ app/src/main/java/vn/edu/usth/usthweather/WeatherActivity.java
@@ -0,0 +1,68 @@
+package vn.edu.usth.usthweather;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class WeatherActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        for (StackTraceElement stelement : Thread.currentThread().getStackTrace()) {
+            Log.i("STLog", stelement.toString());
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        for (StackTraceElement stelement : Thread.currentThread().getStackTrace()) {
+            Log.i("STLog", stelement.toString());
+        }
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        for (StackTraceElement stelement : Thread.currentThread().getStackTrace()) {
+            Log.i("STLog", stelement.toString());
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        for (StackTraceElement stelement : Thread.currentThread().getStackTrace()) {
+            Log.i("STLog", stelement.toString());
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        for (StackTraceElement stelement : Thread.currentThread().getStackTrace()) {
+            Log.i("STLog", stelement.toString());
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        for (StackTraceElement stelement : Thread.currentThread().getStackTrace()) {
+            Log.i("STLog", stelement.toString());
+        }
+    }
+}
